4fb2da888488a2841558e496a360e55bc0490017
hoangle <hoangcisox@gmail.com>
2023-12-07 16:27:37 +0700

    Add ProductCategory CRUD API for Admin

---
+++ aspnet-core/src/HolwnEcommerce.Admin.Application/ProductCategories/ProductCategoriesAppService.cs
@@ -0,0 +1,26 @@
+﻿using HolwnEcommerce.ProductCategories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace HolwnEcommerce.Admin.ProductCategories
+{
+    public class ProductCategoriesAppService : CrudAppService
+        <ProductCategory,
+        ProductCategoryDto, 
+        Guid,
+        PagedResultRequestDto,
+        CreateUpdateProductCategoryDto,
+        CreateUpdateProductCategoryDto>, IProductCategoriesAppService
+    {
+        public ProductCategoriesAppService(IRepository<ProductCategory, Guid> repository) 
+            : base(repository) 
+        {
+        }
+    }
+}
