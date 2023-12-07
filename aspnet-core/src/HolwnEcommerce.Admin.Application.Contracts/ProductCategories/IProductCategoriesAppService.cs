de526c42356cce0ad34ed70c9d1c35d8cf12519a
hoangle <hoangcisox@gmail.com>
2023-12-07 17:07:02 +0700

    Add ListPage for ProductCategory Admin

--- aspnet-core/src/HolwnEcommerce.Admin.Application.Contracts/ProductCategories/IProductCategoriesAppService.cs
+++ aspnet-core/src/HolwnEcommerce.Admin.Application.Contracts/ProductCategories/IProductCategoriesAppService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
@@ -12,5 +13,6 @@ namespace HolwnEcommerce.Admin.ProductCategories
         CreateUpdateProductCategoryDto,
         CreateUpdateProductCategoryDto>
     {
+        Task<PagedResultDto<ProductCategoryInListDto>> GetListFilterAsync(BaseListFilterDto input);
     }
 }
--- aspnet-core/src/HolwnEcommerce.Admin.Application/HolwnEcommerceAdminApplicationAutoMapperProfile.cs
+++ aspnet-core/src/HolwnEcommerce.Admin.Application/HolwnEcommerceAdminApplicationAutoMapperProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using HolwnEcommerce.Admin.ProductCategories;
+using HolwnEcommerce.ProductCategories;
 
 namespace HolwnEcommerce.Admin;
 
@@ -9,5 +11,8 @@ public class HolwnEcommerceAdminApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+        CreateMap<ProductCategory, ProductCategoryDto>();
+        CreateMap<ProductCategory, ProductCategoryInListDto>();
+        CreateMap<CreateUpdateProductCategoryDto, ProductCategory>();
     }
 }
