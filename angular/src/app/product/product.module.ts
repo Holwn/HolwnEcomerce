5340a92730bb8c9179cc71677fd7ea143145abb9
hoangle <hoangcisox@gmail.com>
2023-12-19 17:08:15 +0700

    Add Product List filter in Admin Angular

--- angular/src/app/product/product.module.ts
+++ angular/src/app/product/product.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
 import { SharedModule } from '../shared/shared.module';
-import {PanelModule} from 'primeng/panel';
-import {TableModule} from 'primeng/table';
-import {PaginatorModule} from 'primeng/paginator';
-import {BlockUIModule} from 'primeng/blockui';
+import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
+import { BlockUIModule } from 'primeng/blockui';
+import { ButtonModule } from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [ProductComponent],
-  imports: [SharedModule, ProductRoutingModule, PanelModule, TableModule, PaginatorModule, BlockUIModule],
+  imports: [
+    SharedModule,
+    ProductRoutingModule,
+    PanelModule,
+    TableModule,
+    PaginatorModule,
+    BlockUIModule,
+    ButtonModule,
+    DropdownModule,
+    InputTextModule
+  ],
 })
 export class ProductModule {}
