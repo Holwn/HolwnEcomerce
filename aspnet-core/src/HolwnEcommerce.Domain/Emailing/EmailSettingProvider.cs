5c7dc246119b16761d4dec990cc1763c398da238
Holwn <ltienhoang2@gmail.com>
2024-01-16 09:43:26 +0700

    Add send mail when create cart in client

---
+++ aspnet-core/src/HolwnEcommerce.Domain/Emailing/EmailSettingProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Settings;
+
+namespace HolwnEcommerce.Emailing
+{
+    public class EmailSettingProvider : SettingDefinitionProvider
+    {
+        private readonly ISettingEncryptionService encryptionService;
+
+        public EmailSettingProvider(ISettingEncryptionService encryptionService)
+        {
+            this.encryptionService = encryptionService;
+        }
+
+        public override void Define(ISettingDefinitionContext context)
+        {
+            var passSetting = context.GetOrNull("Abp.Mailing.Smtp.Password");
+            if (passSetting != null)
+            {
+                string debug = encryptionService.Encrypt(passSetting, "78bd09a6bd4a86468cd98e1e658185d9-4c955d28-32099d6b");
+            }
+
+        }
+    }
+}
---
+++ aspnet-core/src/HolwnEcommerce.Domain/Emailing/EmailTemplates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolwnEcommerce.Emailing
+{
+    public static class EmailTemplates
+    {
+        public const string Layout = "HolwnEcommerce.Emailing.Templates.Layout";
+
+        public const string WelcomeEmail = "HolwnEcommerce.Emailing.Templates.WelcomeEmail";
+        public const string CreateOrderEmail = "HolwnEcommerce.Emailing.Templates.CreateOrderEmail";
+    }
+}
