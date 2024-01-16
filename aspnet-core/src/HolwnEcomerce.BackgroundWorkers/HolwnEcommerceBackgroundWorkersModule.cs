5377a03835f3791e0a9e6b9bf64806904ff81b08
Holwn <ltienhoang2@gmail.com>
2024-01-16 13:21:45 +0700

    Add background worker with ABP

---
+++ aspnet-core/src/HolwnEcomerce.BackgroundWorkers/HolwnEcommerceBackgroundWorkersModule.cs
@@ -0,0 +1,53 @@
+﻿using HolwnEcomerce.BackgroundWorkers.MailCampaigns;
+using HolwnEcommerce.EntityFrameworkCore;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Autofac;
+using Volo.Abp.BackgroundWorkers;
+using Volo.Abp.Caching;
+using Volo.Abp.Caching.StackExchangeRedis;
+using Volo.Abp.Modularity;
+
+namespace HolwnEcomerce.BackgroundWorkers
+{
+    [DependsOn(
+      typeof(AbpAutofacModule),
+      typeof(AbpBackgroundWorkersModule),
+      typeof(HolwnEcommerceEntityFrameworkCoreModule),
+      typeof(AbpCachingStackExchangeRedisModule)
+    )]
+    public class HolwnEcommerceBackgroundWorkersModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+            var hostEnvironment = context.Services.GetSingletonInstance<IHostEnvironment>();
+
+            context.Services.AddHostedService<HolwnEcommerceBackgroundWorkersHostedService>();
+
+            Configure<AbpDistributedCacheOptions>(options =>
+            {
+                options.KeyPrefix = "HolwnEcommerce:";
+            });
+            var dataProtectionBuilder = context.Services.AddDataProtection().SetApplicationName("HolwnEcommerce");
+            if (!hostEnvironment.IsDevelopment())
+            {
+                var redis = ConnectionMultiplexer.Connect(configuration["Redis:Configuration"]);
+                dataProtectionBuilder.PersistKeysToStackExchangeRedis(redis, "HolwnEcommerce-Protection-Keys");
+            }
+
+        }
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            context.AddBackgroundWorkerAsync<EmailMarrketingWorker>();
+        }
+    }
+}
